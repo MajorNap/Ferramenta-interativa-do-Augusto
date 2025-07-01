f356c00fd2a6d3cefb1078e5d9126771941ddb94
MajorNap <germano.pyetro@escola.pr.gov.br>
2025-07-01 13:27:05 -0300

    ca

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
-    'Craft table'
+    'Craft table',
     'Qual a utilidade da craft table?',
-    'Basicamente é um bloco interagível que será utilizado do início ao fim do jogo'
+    'Basicamente é um bloco interagível que será utilizado do início ao fim do jogo',
 )
 
 criaCartao(
     'Comandos',
     'Qual o uso dos comandos no Minecraft?'
-    'Os comandos são utilizados para auxiliar durante a jogatina'
+    'Os comandos são utilizados para auxiliar durante a jogatina',
 )
 
 criaCartao(
-    'Vilas
-    'Qual a função das vilas?'
-    'As vila, além de ser um porto seguro para os jogadores, é uma área fundamental de trocas com os moradores de lá'
+    'Vilas',
+    'Qual a função das vilas?',
+    'As vila, além de ser um porto seguro para os jogadores, é uma área fundamental de trocas com os moradores de lá ',
 )
 
 criaCartao(
     'Lingua inglesa',
     'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Oi em ingles é HI (RAI)',
 )
